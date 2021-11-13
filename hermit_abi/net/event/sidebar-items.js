47b17dd7fbcc892979fd266e5bf7eb6ec85d17d8
PJungkamp <PJungkamp@users.noreply.github.com>
2021-11-13 15:33:05 +0000

    Deploy to GitHub pages

---
+++ hermit_abi/net/event/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Event","Event type"],["EventFlags","Flags indicating which event occured"]]});
---
+++ tcp_echo/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["NUM_SOCKETS",""]],"fn":[["main",""]]});
